87f533a8e294b5a121e22783da81ad16c4208257
TyroGames <64827452+TyroGames@users.noreply.github.com>
2022-03-16 23:01:06 -0500

    Add files via upload

---
+++ Data/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial1U.Data
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; } = 0;
+        public string Name { get; set; } = "";
+        public string Gender { get; set; } = "";
+    }
+}
